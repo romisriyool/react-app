5a50d860991068ae7b3819d2ac42e9af71ca3c08
romisriyool <68127288+romisriyool@users.noreply.github.com>
2020-07-24 16:34:23 +0500

    final  commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router basename="/react-app">
+      <Router>
         <Route path={"/"} exact component={Home} />
         <Route path={"/home"} exact component={Home} />
         <Route path={"/about"} exact component={About} />
